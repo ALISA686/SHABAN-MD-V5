ab6d179ad77db1e1533f1b0490c6587f55b23e0a
ALISA686 <alisakate686@gmail.com>
2025-06-27 14:56:57 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~2FVAgCqb#G5hAUludCPZbuOgoCV5aoi25WUn91-gmmB8RAFwWN1I",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
@@ -26,7 +26,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/j9G5tmNM/shaban-
 // add custom menu and mention reply image url   
 BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
+STICKER_NAME: process.env.STICKER_NAME || "𓃠 𝕸𝕴𝕶𝕬𝕰𝕷𝕾𝕺𝕹 𝕷𝕰 𝕱𝕴𝕷𝕾 𝕯𝕰 𝕶𝕳𝕬𝕯𝕴𝕵𝕬 ༆ .  𓆩 𝑶 𝑽 𝑬 𝑹 𝑳 𝑶 𝑹 𝑫 𓆪",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -34,9 +34,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923043788282",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50947494105",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
+OWNER_NAME: process.env.OWNER_NAME || "𓃠 𝕸𝕴𝕶𝕬𝕰𝕷𝕾𝕺𝕹 𝕷𝕰 𝕱𝕴𝕷𝕾 𝕯𝕰 𝕶𝕳𝕬𝕯𝕴𝕵𝕬 ༆ .  𓆩 𝑶 𝑽 𝑬 𝑹 𝑳 𝑶 𝑹 𝑫 𓆪",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
 // add bot owner name    
